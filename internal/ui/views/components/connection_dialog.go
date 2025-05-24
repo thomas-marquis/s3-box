765e5e32fb8d6c4f1abf8af919837e79959ce3d4
Thomas Marquis <thomas.marquis.dev@gmail.com>
2025-05-24 14:29:28 +0200

    refactor as form

--- internal/ui/views/components/connection_dialog.go
+++ internal/ui/views/components/connection_dialog.go
@@ -12,6 +12,32 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+// TODO: move it to utility module
+func getString(data binding.String) string {
+	value, err := data.Get()
+	if err != nil {
+		panic("error while getting string from binding")
+	}
+	return value
+}
+
+func getBool(data binding.Bool) bool {
+	value, err := data.Get()
+	if err != nil {
+		panic("error while getting string from binding")
+	}
+	return value
+}
+
+func getUntypedOrPanic[T any](data binding.Untyped) T {
+	di, _ := data.Get()
+	value, ok := di.(T)
+	if !ok {
+		panic("Invalid connection type")
+	}
+	return value
+}
+
 func makeCopyBtnWithData(enableCopy bool, data binding.String, w fyne.Window) *widget.Button {
 	return widget.NewButtonWithIcon("", theme.ContentCopyIcon(), func() {
 		if enableCopy {
@@ -21,6 +47,17 @@ func makeCopyBtnWithData(enableCopy bool, data binding.String, w fyne.Window) *w
 			}
 		}
 	})
+}
+
+func makeTextFormItemWithData(data binding.String, label, placeholder string, enableCopy bool, w fyne.Window) *widget.FormItem {
+	entry := widget.NewEntryWithData(data)
+	entry.SetPlaceHolder(placeholder)
+	formItem := widget.NewFormItem(label, entry)
+	if enableCopy {
+		entry.ActionItem = makeCopyBtnWithData(enableCopy, data, w)
+	}
+
+	return formItem
 }
 
 func makeTextInputWithData(data binding.String, label, placeholder string, enableCopy bool, w fyne.Window) (*fyne.Container, *widget.Entry) {
@@ -35,13 +72,110 @@ func makeTextInputWithData(data binding.String, label, placeholder string, enabl
 	return c, entry
 }
 
-func NewConnectionDialog(
+func buildAWSForm(
 	ctx appcontext.AppContext,
-	label string,
 	defaultConn connection.Connection,
 	enableCopy bool,
-	onSave func(conn *connection.Connection) error,
-) *dialog.CustomDialog {
+	onSubmit func(conn *connection.Connection) error,
+) *widget.Form {
+	// Init data bindings
+	nameData := binding.NewString()
+	nameData.Set(defaultConn.Name)
+
+	accessKeyData := binding.NewString()
+	accessKeyData.Set(defaultConn.AccessKey)
+
+	secretKeyData := binding.NewString()
+	secretKeyData.Set(defaultConn.SecretKey)
+
+	bucketData := binding.NewString()
+	bucketData.Set(defaultConn.BucketName)
+
+	regionData := binding.NewString()
+	regionData.Set(defaultConn.Region)
+
+	readOnlyData := binding.NewBool()
+	readOnlyData.Set(defaultConn.ReadOnly)
+
+	// Create Form items
+	nameFormItem := makeTextFormItemWithData(
+		nameData,
+		"Connection name",
+		"My new connection",
+		enableCopy,
+		ctx.Window(),
+	)
+	accessKeyFormItem := makeTextFormItemWithData(
+		accessKeyData,
+		"Access key Id",
+		"Access key",
+		enableCopy,
+		ctx.Window(),
+	)
+	secretKeyFormItem := makeTextFormItemWithData(
+		secretKeyData,
+		"Secret access key",
+		"Secret key",
+		enableCopy,
+		ctx.Window(),
+	)
+
+	bucketFormItem := makeTextFormItemWithData(
+		bucketData,
+		"Bucket name",
+		"my-bucket",
+		enableCopy,
+		ctx.Window(),
+	)
+	regionFormItem := makeTextFormItemWithData(
+		regionData,
+		"Region",
+		"us-east-1",
+		enableCopy,
+		ctx.Window(),
+	)
+
+	readOnlyCheckbox := widget.NewCheckWithData("Read only", readOnlyData)
+	readOnlyFormItem := widget.NewFormItem("Read only", readOnlyCheckbox)
+
+	f := widget.NewForm(
+		nameFormItem,
+		accessKeyFormItem,
+		secretKeyFormItem,
+		bucketFormItem,
+		regionFormItem,
+		readOnlyFormItem,
+	)
+	f.OnSubmit = func() {
+		newConn := connection.NewConnection(
+			getString(nameData),
+			getString(accessKeyData),
+			getString(secretKeyData),
+			getString(bucketData),
+			connection.AsAWSConnection(getString(regionData)),
+			connection.WithReadOnlyOption(getBool(readOnlyData)),
+		)
+
+		if err := onSubmit(newConn); err == nil {
+			nameData.Set("")
+			accessKeyData.Set("")
+			secretKeyData.Set("")
+			bucketData.Set("")
+			regionData.Set("")
+			readOnlyData.Set(false)
+		}
+	}
+	f.SubmitText = "Save"
+
+	return f
+}
+
+func buildS3LikeForm(
+	ctx appcontext.AppContext,
+	defaultConn connection.Connection,
+	enableCopy bool,
+	onSubmit func(conn *connection.Connection) error,
+) *widget.Form {
 	// Init data bindings
 	nameData := binding.NewString()
 	nameData.Set(defaultConn.Name)
@@ -58,159 +192,130 @@ func NewConnectionDialog(
 	bucketData := binding.NewString()
 	bucketData.Set(defaultConn.BucketName)
 
-	regionData := binding.NewString()
-	regionData.Set(defaultConn.Region)
-
 	readOnlyData := binding.NewBool()
 	readOnlyData.Set(defaultConn.ReadOnly)
 
 	useTlsData := binding.NewBool()
 	useTlsData.Set(defaultConn.UseTls)
 
-	connectionTypeData := binding.NewUntyped()
-	connectionTypeData.Set(defaultConn.Type)
-
-	nameBloc, nameEntry := makeTextInputWithData(
+	nameFormItem := makeTextFormItemWithData(
 		nameData,
 		"Connection name",
 		"My new connection",
 		enableCopy,
 		ctx.Window(),
 	)
-	accessKeyBloc, accessKeyEntry := makeTextInputWithData(
+	accessKeyFormItem := makeTextFormItemWithData(
 		accessKeyData,
 		"Access key Id",
 		"Access key",
 		enableCopy,
 		ctx.Window(),
 	)
-	secretKeyBloc, secretKeyEntry := makeTextInputWithData(
+	secretKeyFormItem := makeTextFormItemWithData(
 		secretKeyData,
 		"Secret access key",
 		"Secret key",
 		enableCopy,
 		ctx.Window(),
 	)
-	serverBloc, serverEntry := makeTextInputWithData(
+	serverFormItem := makeTextFormItemWithData(
 		serverData,
 		"Server hostname",
 		"s3.amazonaws.com",
 		enableCopy,
 		ctx.Window(),
 	)
-	bucketBloc, bucketEntry := makeTextInputWithData(
+	bucketFormItem := makeTextFormItemWithData(
 		bucketData,
 		"Bucket name",
 		"my-bucket",
 		enableCopy,
 		ctx.Window(),
 	)
-	regionBloc, regionEntry := makeTextInputWithData(
-		regionData,
-		"Region",
-		"us-east-1",
-		enableCopy,
-		ctx.Window(),
-	)
+
 	useTlsCheckbox := widget.NewCheckWithData("Use TLS", useTlsData)
+	useTlsFormItem := widget.NewFormItem("UseTls", useTlsCheckbox)
+
 	readOnlyCheckbox := widget.NewCheckWithData("Read only", readOnlyData)
+	readOnlyFormItem := widget.NewFormItem("Read only", readOnlyCheckbox)
 
-	connTypeRadio := widget.NewRadioGroup([]string{"AWS", "Other"}, func(val string) {
-		var selectedConnectionType connection.ConnectionType
-		switch val {
-		case "AWS":
-			selectedConnectionType = connection.AWSConnectionType
-			useTlsCheckbox.Hide()
-			serverData.Set("")
-			serverBloc.Hide()
-			regionBloc.Show()
-		case "Other":
-			selectedConnectionType = connection.S3LikeConnectionType
-			useTlsCheckbox.Show()
-			serverBloc.Show()
-			regionData.Set("")
-			regionBloc.Hide()
-		default:
-			panic("Unknown connection type")
-		}
-		connectionTypeData.Set(selectedConnectionType)
-	})
-
-	switch defaultConn.Type {
-	case connection.AWSConnectionType:
-		connTypeRadio.SetSelected("AWS")
-	case connection.S3LikeConnectionType:
-		connTypeRadio.SetSelected("Other")
-	}
-
-	// init form
-	saveBtn := widget.NewButton("Save", func() {})
-	saveBtn.SetIcon(theme.ConfirmIcon())
-
-	d := dialog.NewCustom(
-		label,
-		"Close",
-		container.NewVBox(
-			connTypeRadio,
-			nameBloc,
-			serverBloc,
-			accessKeyBloc,
-			secretKeyBloc,
-			bucketBloc,
-			regionBloc,
-			useTlsCheckbox,
-			readOnlyCheckbox,
-			container.NewHBox(saveBtn),
-		),
-		ctx.Window(),
+	// Create form
+	f := widget.NewForm(
+		nameFormItem,
+		accessKeyFormItem,
+		secretKeyFormItem,
+		serverFormItem,
+		bucketFormItem,
+		useTlsFormItem,
+		readOnlyFormItem,
 	)
-	d.Resize(fyne.NewSize(650, 200))
+	f.OnSubmit = func() {
+		newConn := connection.NewConnection(
+			getString(nameData),
+			getString(accessKeyData),
+			getString(secretKeyData),
+			getString(bucketData),
+			connection.AsS3LikeConnection(getString(serverData), getBool(useTlsData)),
+			connection.WithReadOnlyOption(getBool(readOnlyData)),
+		)
 
-	saveBtn.OnTapped = func() {
-		di, _ := connectionTypeData.Get()
-		selectedConnType, ok := di.(connection.ConnectionType)
-		if !ok {
-			panic("Invalid connection type")
-		}
-		isReadOnlySelected, _ := readOnlyData.Get()
-
-		var newConn *connection.Connection
-		switch selectedConnType {
-		case connection.AWSConnectionType:
-			newConn = connection.NewConnection(
-				nameEntry.Text,
-				accessKeyEntry.Text,
-				secretKeyEntry.Text,
-				bucketEntry.Text,
-				connection.AsAWSConnection(regionEntry.Text),
-				connection.WithReadOnlyOption(isReadOnlySelected),
-			)
-		case connection.S3LikeConnectionType:
-			newConn = connection.NewConnection(
-				nameEntry.Text,
-				accessKeyEntry.Text,
-				secretKeyEntry.Text,
-				bucketEntry.Text,
-				connection.AsS3LikeConnection(serverEntry.Text, useTlsCheckbox.Checked),
-				connection.WithReadOnlyOption(isReadOnlySelected),
-			)
-		default:
-			panic("Unknown connection type")
-		}
-
-		if err := onSave(newConn); err == nil {
-			d.Hide()
+		if err := onSubmit(newConn); err == nil {
 			nameData.Set("")
 			accessKeyData.Set("")
 			secretKeyData.Set("")
 			serverData.Set("")
 			bucketData.Set("")
-			regionData.Set("")
 			useTlsData.Set(false)
-			connectionTypeData.Set(connection.AWSConnectionType)
 			readOnlyData.Set(false)
 		}
 	}
+	f.SubmitText = "Save"
+
+	return f
+}
+
+func NewConnectionDialog(
+	ctx appcontext.AppContext,
+	label string,
+	defaultConn connection.Connection,
+	enableCopy bool,
+	onSave func(conn *connection.Connection) error,
+) dialog.Dialog {
+	var d dialog.Dialog
+
+	handleOnSubmit := func(c *connection.Connection) error {
+		err := onSave(c)
+		d.Hide()
+		return err
+	}
+
+	tabs := container.NewAppTabs(
+		container.NewTabItem(
+			"AWS",
+			container.NewVBox(
+				widget.NewLabel(""),
+				buildAWSForm(ctx, defaultConn, enableCopy, handleOnSubmit),
+			),
+		),
+		container.NewTabItem(
+			"Other (S3 Like)",
+			container.NewVBox(
+				widget.NewLabel(""),
+				buildS3LikeForm(ctx, defaultConn, enableCopy, handleOnSubmit),
+			),
+		),
+	)
+
+	switch defaultConn.Type {
+	case connection.AWSConnectionType:
+		tabs.SelectIndex(0)
+	case connection.S3LikeConnectionType:
+		tabs.SelectIndex(1)
+	}
+
+	d = dialog.NewCustom(label, "cancel", tabs, ctx.Window())
+	d.Resize(fyne.NewSize(650, 200))
 
 	return d
 }
