8f85d951997d23adc92195908de6d0796efdab6f
Thomas Marquis <thomas.marquis.dev@gmail.com>
2026-01-21 11:30:46 +0100

    notification is now clickable

--- internal/ui/views/notification.go
+++ internal/ui/views/notification.go
@@ -4,20 +4,64 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/dialog"
 	fyne_widget "fyne.io/fyne/v2/widget"
 	appcontext "github.com/thomas-marquis/s3-box/internal/ui/app/context"
 	"github.com/thomas-marquis/s3-box/internal/ui/views/widget"
 )
 
+type tappableLabel struct {
+	*fyne_widget.Label
+
+	window fyne.Window
+}
+
+var (
+	_ fyne.Widget         = (*tappableLabel)(nil)
+	_ fyne.DoubleTappable = (*tappableLabel)(nil)
+	_ fyne.Tappable       = (*tappableLabel)(nil)
+)
+
+func newTappableLabel(text string, window fyne.Window) *tappableLabel {
+	l := &tappableLabel{fyne_widget.NewLabel(text), window}
+	l.ExtendBaseWidget(l)
+	return l
+}
+
+func (l *tappableLabel) DoubleTapped(*fyne.PointEvent) {
+	l.handleTape()
+}
+
+func (l *tappableLabel) Tapped(*fyne.PointEvent) {
+	l.handleTape()
+}
+
+func (l *tappableLabel) handleTape() {
+	content := fyne_widget.NewLabel(l.Text)
+	content.Wrapping = fyne.TextWrapWord
+	content.Alignment = fyne.TextAlignLeading
+	content.Selectable = true
+
+	d := dialog.NewCustom("", "Ok", content, l.window)
+	d.Resize(fyne.NewSize(600, 300))
+	d.Show()
+}
+
 func GetNotificationView(appCtx appcontext.AppContext) (*fyne.Container, error) {
 	notificationList := fyne_widget.NewListWithData(
 		appCtx.NotificationViewModel().Notifications(),
 		func() fyne.CanvasObject {
-			return fyne_widget.NewTextGridFromString("")
+			label := newTappableLabel("", appCtx.Window())
+			label.Alignment = fyne.TextAlignLeading
+			label.Truncation = fyne.TextTruncateEllipsis
+			label.TextStyle = fyne.TextStyle{Monospace: true}
+			label.Selectable = false
+			return label
 		},
 		func(di binding.DataItem, obj fyne.CanvasObject) {
-			value, _ := di.(binding.String).Get()
-			obj.(*fyne_widget.TextGrid).SetText(value)
+			value, _ := di.(binding.String)
+			label := obj.(*tappableLabel)
+			label.Bind(value)
 		},
 	)
 
