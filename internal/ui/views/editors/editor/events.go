6b7fefc62c7bfdbc75e43b0c6d4469b36c6d7c04
Thomas Marquis <thomas.marquis.dev@gmail.com>
2026-07-30 19:23:21 +0200

    add new event types

--- internal/ui/views/editors/editor/events.go
+++ internal/ui/views/editors/editor/events.go
@@ -9,7 +9,96 @@ const (
 	SaveTriggeredType event.Type = "event.fileeditor.save.triggered"
 	SaveSucceededType event.Type = "event.fileeditor.save.succeeded"
 	SaveFailedType    event.Type = "event.fileeditor.save.failed"
+
+	/////
+
+	LoadedType          event.Type = "event.editor.loaded"
+	LoadFailedType      event.Type = "event.editor.load.failed"
+	ClosedType          event.Type = "event.editor.closed"
+	CloseRequestedTyped event.Type = "event.editor.close.requested"
+	CloseConfirmedType  event.Type = "event.editor.close.confirmed"
+	CloseCanceledType   event.Type = "event.editor.close.canceled"
 )
+
+type Event interface {
+	This() Editor
+}
+
+type Loaded struct {
+	Editor  Editor
+	Content directory.FileContent
+}
+
+func (Loaded) EventType() event.Type {
+	return LoadedType
+}
+
+func (p Loaded) This() Editor {
+	return p.Editor
+}
+
+type LoadFailed struct {
+	Editor Editor
+	Err    error
+}
+
+func (LoadFailed) EventType() event.Type {
+	return LoadFailedType
+}
+
+func (p LoadFailed) This() Editor {
+	return p.Editor
+}
+
+type Closed struct {
+	Editor Editor
+}
+
+func (Closed) EventType() event.Type {
+	return ClosedType
+}
+
+func (p Closed) This() Editor {
+	return p.Editor
+}
+
+type CloseRequested struct {
+	Editor Editor
+}
+
+func (CloseRequested) EventType() event.Type {
+	return CloseRequestedTyped
+}
+
+func (p CloseRequested) This() Editor {
+	return p.Editor
+}
+
+type CloseConfirmed struct {
+	Editor Editor
+}
+
+func (CloseConfirmed) EventType() event.Type {
+	return CloseConfirmedType
+}
+
+func (p CloseConfirmed) This() Editor {
+	return p.Editor
+}
+
+type CloseCanceled struct {
+	Editor Editor
+}
+
+func (CloseCanceled) EventType() event.Type {
+	return CloseCanceledType
+}
+
+func (p CloseCanceled) This() Editor {
+	return p.Editor
+}
+
+/////
 
 type SaveTriggered struct {
 	File    *directory.File
