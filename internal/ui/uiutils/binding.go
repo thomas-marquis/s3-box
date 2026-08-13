f125ec0507b141d03fbdd6e5707cbf8893f3645f
Thomas Marquis <thomas.marquis.dev@gmail.com>
2026-08-13 10:02:27 +0200

    remove all Untyped binding

--- internal/ui/uiutils/binding.go
+++ internal/ui/uiutils/binding.go
@@ -1,7 +1,6 @@
 package uiutils
 
 import (
-	"fmt"
 	"runtime"
 	"time"
 
@@ -27,42 +26,6 @@ func GetBool(data binding.Bool) bool {
 		panic("error while getting string from binding")
 	}
 	return value
-}
-
-func GetUntypedListOrPanic[T any](data binding.UntypedList) []T {
-	dis, err := data.Get()
-	if err != nil {
-		panic("error while getting value from binding")
-	}
-
-	values := make([]T, len(dis))
-	for i, di := range dis {
-		value, ok := di.(T)
-		if !ok {
-			panic("Invalid casting type for binding.UntypedList")
-		}
-		values[i] = value
-	}
-
-	return values
-}
-
-func GetUntypedList[T any](data binding.UntypedList) ([]T, error) {
-	items, err := data.Get()
-	if err != nil {
-		return nil, err
-	}
-
-	values := make([]T, len(items))
-	for i, item := range items {
-		value, ok := item.(T)
-		if !ok {
-			return nil, fmt.Errorf("invalid casting type for binding.UntypedList: expected %T, got %T", value, item)
-		}
-		values[i] = value
-	}
-
-	return values, nil
 }
 
 type BindingItemFormatter[T any] struct {
